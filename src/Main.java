b147514b784b88a5ff0d92870f93240dff9c0c71
João Vitor Eliazar <jvearocha@gmail.com>
2023-04-19 22:50:39 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static final double SALARIO_MINIMO = 1320;
-    public static final String NOME_COLABORADOR = printaNome();
-    public static final String CARGO_COLABORADOR = CargoColaborador();
-    public static final String DATA_ADMISSAO = DataAdmissao();
-    public static final String MES_REFERENCIA = MesReferencia();
-    public static final double SALARIO_BRUTO = CalculoSalarioBruto();
-    public static final int DIAS_TRABALHADOS_SEMANA = DiasTrabalhadosSemana();
-    public static final double SALARIO_HORA = CalculoSalarioHora();
-    public static final boolean DIREITO_PERICULOSIDADE = DireitoPericulosidade();
-    public static final double CALCULO_INSALUBRIDADE = CalculoInsalubridade();
-    public static final double VALE_TRANSPORTE = CalculoValeTransporte();
-    public static final double VALE_ALIMENTACAO = CalculoValeAlimentacao();
-    public static final double CALCULO_IRRF = CalculoDescontoIRRF();
     public static String printaNome(){
         Scanner scan = new Scanner(System.in);
         System.out.print("Digite seu nome completo: ");
-        String nome = scan.nextLine();
-        return nome;
+        return scan.nextLine();
     }
     public static String CargoColaborador(){
         Scanner scan = new Scanner(System.in);
         System.out.print("Qual é o seu cargo? ");
-        String cargo = scan.nextLine();
-        return cargo;
+        return scan.nextLine();
     }
     public static String DataAdmissao() {
         Scanner scan = new Scanner(System.in);
@@ -53,8 +39,7 @@ public class Main {
             if(ano<2016 || ano>2023)
                 j = 0;
         }
-        String data = dia+"/"+mes+"/"+ano;
-        return data;
+        return dia+"/"+mes+"/"+ano;
     }
     public static String MesReferencia(){
         Scanner scan = new Scanner(System.in);
@@ -64,73 +49,70 @@ public class Main {
             System.out.println("Qual é o mês de referência da folha de pagamento? (Responda em números de 1 a 12)");
             int nmes = scan.nextInt();
             switch (nmes) {
-                case 1:
+                case 1 -> {
                     mes = "Janeiro";
                     x = 1;
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     mes = "Fevereiro";
                     x = 1;
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     mes = "Março";
                     x = 1;
-                    break;
-                case 4:
+                }
+                case 4 -> {
                     mes = "Abril";
                     x = 1;
-                    break;
-                case 5:
+                }
+                case 5 -> {
                     mes = "Maio";
                     x = 1;
-                    break;
-                case 6:
+                }
+                case 6 -> {
                     mes = "Junho";
                     x = 1;
-                    break;
-                case 7:
+                }
+                case 7 -> {
                     mes = "Julho";
                     x = 1;
-                    break;
-                case 8:
+                }
+                case 8 -> {
                     mes = "Agosto";
                     x = 1;
-                    break;
-                case 9:
+                }
+                case 9 -> {
                     mes = "Setembro";
                     x = 1;
-                    break;
-                case 10:
+                }
+                case 10 -> {
                     mes = "Outubro";
                     x = 1;
-                    break;
-                case 11:
+                }
+                case 11 -> {
                     mes = "Novembro";
                     x = 1;
-                    break;
-                case 12:
+                }
+                case 12 -> {
                     mes = "Dezembro";
                     x = 1;
-                    break;
-                default:
-                    System.out.println("Mês inválido!");
+                }
+                default -> System.out.println("Mês inválido!");
             }
         }
         return mes;
     }
-    public static double CalculoSalarioBruto(){
+    public static double CalculoSalarioBruto(String nomeColaborador){
         Scanner scan = new Scanner(System.in);
-        System.out.print(NOME_COLABORADOR+", qual é seu salário bruto? R$");
-        double SalarioBruto = scan.nextDouble();
-        return SalarioBruto;
+        System.out.print(nomeColaborador+", qual é seu salário bruto? R$");
+        return scan.nextDouble();
     }
     public static int DiasTrabalhadosSemana(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Você trabalha quantos dias por semana? ");
-        int DiasTrabalhadosSemana = scan.nextInt();
-        return DiasTrabalhadosSemana;
+        return scan.nextInt();
     }
-    public static double CalculoSalarioHora(){
+    public static double CalculoSalarioHora(double salarioBruto, int diasTrabalhadosSemana){
         Scanner scan = new Scanner(System.in);
         System.out.println("Que horas você começa a trabalhar?(ex: 8) ");
         int HoraInicial = scan.nextInt();
@@ -139,10 +121,9 @@ public class Main {
         int CargaHorariaDiaria = HoraFinal - HoraInicial;
         System.out.println("Qual é a duração em horas do seu período de descanso diariamente? ");
         int PeriodoDescansoDiario = scan.nextInt();
-        int CHSemanal = (CargaHorariaDiaria-PeriodoDescansoDiario)*DIAS_TRABALHADOS_SEMANA;
+        int CHSemanal = (CargaHorariaDiaria-PeriodoDescansoDiario)*diasTrabalhadosSemana;
         int CHMensal = CHSemanal*5;
-        double SalarioHora = SALARIO_BRUTO/CHMensal;
-        return SalarioHora;
+        return salarioBruto/CHMensal;
     }
     public static boolean DireitoPericulosidade(){
         Scanner scan = new Scanner(System.in);
@@ -161,13 +142,13 @@ public class Main {
         }
         return retorno;
     }
-    public static double CalculoInsalubridade(){
+    public static double CalculoInsalubridade(String nomeColaborador){
         Scanner scan = new Scanner(System.in);
         int x = 0;
         int y = 0;
         double retorno = 0;
         while(x == 0) {
-            System.out.println(NOME_COLABORADOR + ", você tem direito ao adicional de insalubridade? (Digite 1 para 'Sim' e 2 para 'Não') ");
+            System.out.println(nomeColaborador + ", você tem direito ao adicional de insalubridade? (Digite 1 para 'Sim' e 2 para 'Não') ");
             int resp = scan.nextInt();
             if(resp == 1){
                 y = 1;
@@ -193,10 +174,10 @@ public class Main {
         }
         return retorno;
     }
-    public static double CalculoValeTransporte(){
+    public static double CalculoValeTransporte(double salarioBruto){
         Scanner scan = new Scanner(System.in);
         double retorno = 0;
-        double limite = SALARIO_BRUTO*0.06;
+        double limite = salarioBruto*0.06;
         System.out.print("Quanto você recebeu de vale transporte? R$");
         double resp = scan.nextDouble();
         if(resp<=limite)
@@ -204,35 +185,35 @@ public class Main {
         else retorno = limite;
         return retorno;
     }
-    public static double CalculoValeAlimentacao(){
-        int CHMensal = DIAS_TRABALHADOS_SEMANA*5;
+    public static double CalculoValeAlimentacao(int diasTrabalhadosSemana){
+        int CHMensal = diasTrabalhadosSemana*5;
         return CHMensal*24;
     }
-    public static double CalculoDescontoINSS(){
+    public static double CalculoDescontoINSS(double salarioBruto){
         double DescontoTotal = 0;
         double faixa1 = 97.65;
         double faixa2 = 114.24;
         double faixa3 = 154.28;
         double faixa4 = 511.07;
-        if(SALARIO_BRUTO<=1302)
-            DescontoTotal = SALARIO_BRUTO*0.075;
-        if(SALARIO_BRUTO>1302 && SALARIO_BRUTO<=2571.29)
-            DescontoTotal = faixa1 + ((SALARIO_BRUTO-1302)*0.09);
-        if(SALARIO_BRUTO>2571.29 && SALARIO_BRUTO<=3856.94)
-            DescontoTotal = faixa1+faixa2+((SALARIO_BRUTO-2571.29)*0.12);
-        if(SALARIO_BRUTO>3856.94 && SALARIO_BRUTO<=7507.49)
-            DescontoTotal = faixa1+faixa2+faixa3+((SALARIO_BRUTO-3856.94)*0.14);
-        if(SALARIO_BRUTO>7507.49)
+        if(salarioBruto<=1302)
+            DescontoTotal = salarioBruto*0.075;
+        if(salarioBruto>1302 && salarioBruto<=2571.29)
+            DescontoTotal = faixa1 + ((salarioBruto-1302)*0.09);
+        if(salarioBruto>2571.29 && salarioBruto<=3856.94)
+            DescontoTotal = faixa1+faixa2+((salarioBruto-2571.29)*0.12);
+        if(salarioBruto>3856.94 && salarioBruto<=7507.49)
+            DescontoTotal = faixa1+faixa2+faixa3+((salarioBruto-3856.94)*0.14);
+        if(salarioBruto>7507.49)
             DescontoTotal = faixa1+faixa2+faixa3+faixa4;
         return DescontoTotal;
     }
-    public static double CalculoDescontoFGTS(){
-        return SALARIO_BRUTO * 0.08;
+    public static double CalculoDescontoFGTS(double salarioBruto){
+        return salarioBruto * 0.08;
     }
-    public static double CalculoDescontoIRRF(){
+    public static double CalculoDescontoIRRF(double salarioBruto){
         Scanner scan = new Scanner(System.in);
         double DescontoTotal = 0, vdep;
-        double SalarioBase = SALARIO_BRUTO-CalculoDescontoINSS();
+        double SalarioBase = salarioBruto-CalculoDescontoINSS(salarioBruto);
         System.out.print("Quantos dependentes você possui? ");
         double dep = scan.nextDouble();
         if(dep>0) {
@@ -249,21 +230,21 @@ public class Main {
             DescontoTotal = (SalarioBase*0.275)-869.36;
         return DescontoTotal;
     }
-    public static double CalculoSalarioLiquido(){
-        double SalarioLiquido = SALARIO_BRUTO;
-        double addPericulosidade = SALARIO_BRUTO * 0.3;
-        if(DIREITO_PERICULOSIDADE)
+    public static double CalculoSalarioLiquido(double salarioBruto, boolean direitoPericulosidade,double calculoInsalubridade, double valeTransporte, double calculoIRRF){
+        double SalarioLiquido = salarioBruto;
+        double addPericulosidade = salarioBruto * 0.3;
+        if(direitoPericulosidade)
             SalarioLiquido += addPericulosidade;
-        SalarioLiquido += CALCULO_INSALUBRIDADE;
-        SalarioLiquido -= VALE_TRANSPORTE;
-        SalarioLiquido -= CalculoDescontoINSS();
-        SalarioLiquido -= CalculoDescontoFGTS();
-        SalarioLiquido -= CALCULO_IRRF;
+        SalarioLiquido += calculoInsalubridade;
+        SalarioLiquido -= valeTransporte;
+        SalarioLiquido -= CalculoDescontoINSS(salarioBruto);
+        SalarioLiquido -= CalculoDescontoFGTS(salarioBruto);
+        SalarioLiquido -= calculoIRRF;
 
         return SalarioLiquido;
     }
-    public static void Relatorio(){
-        double Periculosidade = SALARIO_BRUTO * 0.3;
+    public static void Relatorio(String nomeColaborador, String cargoColaborador, String dataAdmissao, String mesReferencia, double salarioBruto, double salarioHora,boolean direitoPericulosidade, double calculoInsalubridade, double valeTransporte, double valeAlimentacao, double calculoIRRF){
+        double Periculosidade = salarioBruto * 0.3;
         /*
          Informações Obrigatórias da Folha de Pagamento
          - Nome do colaborador;
@@ -279,30 +260,42 @@ public class Main {
          - Base de cálculo INSS/FGTS/IRRF.
          */
         System.out.println("\n\n\nRelatório:\n\n\n");
-        System.out.println("Nome do colaborador: "+NOME_COLABORADOR);
-        System.out.println("Data de admissão: "+DATA_ADMISSAO);
-        System.out.println("Mês de Referência: "+MES_REFERENCIA);
-        System.out.println("Cargo do colaborador: "+CARGO_COLABORADOR);
-        System.out.printf("O salário do colaborador é R$%.2f\n",SALARIO_BRUTO);
+        System.out.println("Nome do colaborador: "+nomeColaborador);
+        System.out.println("Data de admissão: "+dataAdmissao);
+        System.out.println("Mês de Referência: "+mesReferencia);
+        System.out.println("Cargo do colaborador: "+cargoColaborador);
+        System.out.printf("O salário do colaborador é R$%.2f\n",salarioBruto);
         //proventos
-        System.out.printf("\nPROVENTOS:\n1- Salário Bruto: R$%.2f\n2- Vale Refeição(Benefício Gratuito): R$%.2f\n",SALARIO_BRUTO,VALE_ALIMENTACAO);
-        if(CALCULO_INSALUBRIDADE>0)
-            System.out.printf("3- Insalubridade: R$%.2f\n", CALCULO_INSALUBRIDADE);
-        if(DIREITO_PERICULOSIDADE == true)
+        System.out.printf("\nPROVENTOS:\n1- Salário Bruto: R$%.2f\n2- Vale Refeição(Benefício Gratuito): R$%.2f\n",salarioBruto,valeAlimentacao);
+        if(calculoInsalubridade>0)
+            System.out.printf("3- Insalubridade: R$%.2f\n", calculoInsalubridade);
+        if(direitoPericulosidade)
             System.out.printf("4- Periculosidade: R$%.2f\n",Periculosidade);
         //descontos
-        System.out.printf("\nDESCONTOS:\n1- INSS: R$%.2f\n2- IRRF: R$%.2f\n3- FGTS: R$%.2f\n4- Vale Transporte: R$%.2f",CalculoDescontoINSS(),CALCULO_IRRF, CalculoDescontoFGTS(), VALE_TRANSPORTE);
-        System.out.printf("\nValor bruto do salário: R$%.2f\n",SALARIO_BRUTO);
-        System.out.printf("Valor do salário por hora: R$%.2f\n", SALARIO_HORA);
-        System.out.printf("Salário líquido do colaborador: R$%.2f\n", CalculoSalarioLiquido());
-        double CalcIRRF = SALARIO_BRUTO - CALCULO_IRRF;
-        double CalcINSS = CalculoDescontoINSS();
-        double CalcFGTS = CalculoDescontoFGTS();
-        System.out.printf("Base de cálculo INSS: R$%.2f - R$%.2f = R$%.2f\n",SALARIO_BRUTO,CalcINSS,(SALARIO_BRUTO-CalcINSS));
-        System.out.printf("Base de cálculo FGTS: R$%.2f - R$%.2f = R$%.2f\n",SALARIO_BRUTO,CalcFGTS,(SALARIO_BRUTO-CalcFGTS));
-        System.out.printf("Base de cálculo IRRF: R$%.2f - R$%.2f = R$%.2f\n",SALARIO_BRUTO,CALCULO_IRRF,CalcIRRF);
+        System.out.printf("\nDESCONTOS:\n1- INSS: R$%.2f\n2- IRRF: R$%.2f\n3- FGTS: R$%.2f\n4- Vale Transporte: R$%.2f\n",CalculoDescontoINSS(salarioBruto),calculoIRRF, CalculoDescontoFGTS(salarioBruto), valeTransporte);
+        System.out.printf("\nValor bruto do salário: R$%.2f\n",salarioBruto);
+        System.out.printf("Valor do salário por hora: R$%.2f\n", salarioHora);
+        System.out.printf("Valor líquido do salário: R$%.2f\n", CalculoSalarioLiquido(salarioBruto,direitoPericulosidade,calculoInsalubridade,valeTransporte, calculoIRRF));
+        double CalcIRRF = salarioBruto - calculoIRRF;
+        double CalcINSS = CalculoDescontoINSS(salarioBruto);
+        double CalcFGTS = CalculoDescontoFGTS(salarioBruto);
+        System.out.printf("Base de cálculo INSS: R$%.2f - R$%.2f = R$%.2f\n",salarioBruto,CalcINSS,(salarioBruto-CalcINSS));
+        System.out.printf("Base de cálculo FGTS: R$%.2f - R$%.2f = R$%.2f\n",salarioBruto,CalcFGTS,(salarioBruto-CalcFGTS));
+        System.out.printf("Base de cálculo IRRF: R$%.2f - R$%.2f = R$%.2f\n",salarioBruto,calculoIRRF,CalcIRRF);
     }
     public static void main(String[] args) {
-        Relatorio();
+        String nomeColaborador = printaNome();
+        String cargoColaborador = CargoColaborador();
+        String dataAdmissao = DataAdmissao();
+        String mesReferencia = MesReferencia();
+        double salarioBruto = CalculoSalarioBruto(nomeColaborador);
+        int diasTrabalhadosSemana = DiasTrabalhadosSemana();
+        double salarioHora = CalculoSalarioHora(salarioBruto,diasTrabalhadosSemana);
+        boolean direitoPericulosidade = DireitoPericulosidade();
+        double calculoInsalubridade = CalculoInsalubridade(nomeColaborador);
+        double valeTransporte = CalculoValeTransporte(salarioBruto);
+        double valeAlimentacao = CalculoValeAlimentacao(diasTrabalhadosSemana);
+        double calculoIRRF = CalculoDescontoIRRF(salarioBruto);
+        Relatorio(nomeColaborador,cargoColaborador,dataAdmissao,mesReferencia, salarioBruto, salarioHora, direitoPericulosidade, calculoInsalubridade, valeTransporte, valeAlimentacao,calculoIRRF);
     }
 }
